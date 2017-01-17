bf46cb7df2c7363c94e26cd003304072b3fb6912
RJ Mohammad <rjmohammad@outlook.com>
2017-01-17 01:47:25 -0500

    got cards to render

--- client/app/animeDetail/details.js
+++ client/app/animeDetail/details.js
@@ -1,8 +1,12 @@
 angular.module('app.details', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-.controller('DetailsCtrl', function ($scope ) {
+.controller('DetailsCtrl', function ($scope, Anime ) {
+    $scope.dataArray = []
     $scope.fetch = function() {
-        
+        Anime.getData().then(function (data) {
+             $scope.dataArray = data.data
+        })
     }
+    $scope.fetch();
 }).config(function($mdThemingProvider){
    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
 })
